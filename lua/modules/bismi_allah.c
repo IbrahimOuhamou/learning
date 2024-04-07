afbd959a51a25935d7ffb4464263c763efb79341
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-07 12:18:35 +0000

    alhamdo li Allah tweaked some stuff in the lua lib

--- lua/modules/bismi_allah.c
+++ lua/modules/bismi_allah.c
@@ -3,13 +3,33 @@
 
 #include <lua.h>
 
+int bismi_allah(lua_State *L);
+
 int luaopen_bismi_allah(lua_State *L)
 {
+
+    lua_newtable(L);
+    int bismi_allah_index = lua_gettop(L);
+
     lua_pushstring(L, "بسم الله الرحمن الرحيم");
-    lua_setglobal(L, "bismi_allah");
+    lua_setfield(L, bismi_allah_index, "bismi_allah");
     lua_pushstring(L, "la ilaha illa Allah mohammed rassoul Allah");
-    lua_setglobal(L, "shahada");
-    return 0;
+    lua_setfield(L, bismi_allah_index, "shahada");
+
+    lua_pushcfunction(L, bismi_allah);
+    lua_setglobal(L, "open_bismi_allah");
+
+    return 1;
 }
 
+
+int bismi_allah(lua_State *L)
+{
+    luaopen_bismi_allah(L);
+    lua_setglobal(L, "bismi_allah_module");
+
+    lua_getglobal(L, "bismi_allah_module");
+    return 1;
+
+}
 
--- lua/modules/bismi_allah_lua.lua
+++ lua/modules/bismi_allah_lua.lua
@@ -1,7 +1,14 @@
 --بسم الله الرحمن الرحيم
 --la ilaha illa Allah mohammed rassoul Allah
 
-require("bismi_allah")
-print(bismi_allah)
-print(shahada)
+bismi_allah = require("bismi_allah")
+
+--bismi_allah_module.bismi_allah = "in the name of Allah"
+
+print('bismi_allah.bismi_allah    == ' .. bismi_allah.bismi_allah)
+print('bismi_allah.shahada        == ' .. bismi_allah.shahada)
+
+print(type(bismi_allah_module))
+open_bismi_allah()
+print('bismi_allah_module.shahada == ' .. bismi_allah_module.shahada)
 
