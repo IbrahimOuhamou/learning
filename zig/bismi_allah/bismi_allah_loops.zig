5b80c627705f232f6a3dcdca7fb890fea21e33fc
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-21 19:08:47 +0100

    la ilaha illa Allah

--- zig/bismi_allah/bismi_allah_loops.zig
+++ zig/bismi_allah/bismi_allah_loops.zig
@@ -6,11 +6,13 @@ const std = @import("std");
 pub fn main() void {
     std.debug.print("بسم الله الرحمن الرحيم\n", .{});
 
-    const bismi_allah_arr = [_]u8{ 12, 13, 31, 13, 0 };
+    const bismi_allah_arr: [5]u8 = [_]u8{ 2, 12, 13, 1, 5 };
+    var bismi_allah_min: u8 = bismi_allah_arr[0];
     for (bismi_allah_arr) |value| {
-        if (value == 0) {
-            break;
+        std.debug.print("alhamdo li Allah min:{} value:{}\n", .{ bismi_allah_min, value });
+        if (value < bismi_allah_min) {
+            bismi_allah_min = value;
         }
-        std.debug.print("alhamdo li Allah\n", .{});
     }
+    std.debug.print("alhamdo li Allah bismi_allah_min=={}\n", .{bismi_allah_min});
 }
---
+++ zig/bismi_allah_vector/bismi_allah.mk
@@ -0,0 +1,5 @@
+بسم الله الرحمن الرحيم
+la ilaha illa Allah mohammed rassoul Allah
+
+by the will of Allah will try tro make a vector library
+
