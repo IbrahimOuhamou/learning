5c834f64806e06ec6ca2a5b448e5480363bf2957
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-24 21:21:37 +0100

    alhamdo li Allah added zig-c-sdl-nk

--- zig/c/sdl/src/bismi_allah.c
+++ zig/c/sdl/src/bismi_allah.c
@@ -1,5 +1,6 @@
 //in the name of Allah
 
+/*
 #include <stdio.h>
 #include <SDL2/SDL.h>
 
@@ -12,4 +13,134 @@ int main(int argc, char *argv[])
 {
     printf("بسم الله الرحمن الرحيم\n");
 }
+*/
+
+
+//in the name of Allah
+//la ilaha illa Allah mohammed rassoul Allah
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <limits.h>
+#include <time.h>
+
+#include <SDL2/SDL.h>
+
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_SDL_RENDERER_IMPLEMENTATION
+#include "../submodules/Nuklear/nuklear.h"
+#include "../submodules/Nuklear/demo/sdl_renderer/nuklear_sdl_renderer.h"
+
+int main(int argc, char *argv[])
+{
+    printf("in the name of Allah\n");
+    printf("la ilaha illa Allah mohammed rassoul Allah\n");
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Window *window = SDL_CreateWindow("bismi_allah", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 600,  SDL_WINDOW_RESIZABLE);
+    if(NULL == window)
+    {
+        SDL_Log("Error SDL_CreateWindow %s", SDL_GetError());
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
+    if(NULL == window)
+    {
+        SDL_Log("Error SDL_CreateRenderer %s", SDL_GetError());
+    }
+
+    float font_scale = 1;
+    struct nk_context *nk_ctx;
+    struct nk_colorf bg;
+    /* scale the renderer output for High-DPI displays */
+    {
+        int render_w, render_h;
+        int window_w, window_h;
+        float scale_x, scale_y;
+        SDL_GetRendererOutputSize(renderer, &render_w, &render_h);
+        SDL_GetWindowSize(window, &window_w, &window_h);
+        scale_x = (float)(render_w) / (float)(window_w);
+        scale_y = (float)(render_h) / (float)(window_h);
+        SDL_RenderSetScale(renderer, scale_x, scale_y);
+        font_scale = scale_y;
+    }
+
+    nk_ctx = nk_sdl_init(window, renderer);
+    {
+        struct nk_font_atlas *atlas;
+        struct nk_font_config config = nk_font_config(0);
+        struct nk_font *font;
+
+        /* set up the font atlas and add desired font; note that font sizes are
+         * multiplied by font_scale to produce better results at higher DPIs */
+        nk_sdl_font_stash_begin(&atlas);
+        font = nk_font_atlas_add_default(atlas, 13 * font_scale, &config);
+        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/DroidSans.ttf", 14 * font_scale, &config);*/
+        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Roboto-Regular.ttf", 16 * font_scale, &config);*/
+        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/kenvector_future_thin.ttf", 13 * font_scale, &config);*/
+        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyClean.ttf", 12 * font_scale, &config);*/
+        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/ProggyTiny.ttf", 10 * font_scale, &config);*/
+        /*font = nk_font_atlas_add_from_file(atlas, "../../../extra_font/Cousine-Regular.ttf", 13 * font_scale, &config);*/
+        nk_sdl_font_stash_end();
+
+        /* this hack makes the font appear to be scaled down to the desired
+         * size and is only necessary when font_scale > 1 */
+        font->handle.height /= font_scale;
+        /*nk_style_load_all_cursors(ctx, atlas->cursors);*/
+        nk_style_set_font(nk_ctx, &font->handle);
+    }
+
+    int running = 1;
+    while(running)
+    {
+        SDL_Event event;
+        nk_input_begin(nk_ctx);
+        while (SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_QUIT: running = 0; break;
+            }
+            nk_sdl_handle_event(&event);
+        }
+        nk_input_end(nk_ctx);
+
+        //GUI
+        if(nk_begin(nk_ctx, "in the name of Allah", nk_rect(50, 50, 200, 200), NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE))
+        {
+            nk_layout_row_dynamic(nk_ctx, 30, 1);
+            if(nk_button_label(nk_ctx, "bismi Allah 1"))
+            {
+                printf("[nk] pressed 'bismi Allah' button\n");
+            }
+        }nk_end(nk_ctx);
+
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderClear(renderer);
+
+        nk_sdl_render(NK_ANTI_ALIASING_ON);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    nk_sdl_shutdown();
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
+
+
 
