1d27d69596cee988a4ea4eea8fb0cee8785ccfa3
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-24 12:57:05 +0100

    alhamdo li Allah

---
+++ zig/c/build.zig
@@ -0,0 +1,4 @@
+//بسم الله الرحمن الرحيم
+//la ilaha illa Allah mohammed rassoul Allah
+
+//if Allah wills I will try to compile using zig's build system
---
+++ zig/capy/src/main.zig
@@ -0,0 +1,8 @@
+//بسم الله الرحمن الرحيم
+//la ilaha illa Allah mohammed rassoul Allah
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("بسم الله الرحمن الرحيم\n", .{});
+    std.debug.print("la ilaha illa Allah mohammed rassoul Allah\n", .{});
+}
