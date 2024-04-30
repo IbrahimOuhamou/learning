fdd96827268261711ce2285b4341b116e6053fb3
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-30 11:19:52 +0100

    alhamdo li Allah

--- react/bismi_allah_app/src/App.js
+++ react/bismi_allah_app/src/App.js
@@ -1,5 +1,13 @@
+//بسم الله الرحمن الرحيم
+//la ilaha illa Allah mohammed rassoul Allah
 import logo from './logo.svg';
 import './App.css';
+
+let bismi_allah = {
+  text: 'in the name of Allah',
+  bgc: "black",
+  fgc: "white",
+};
 
 function BismiAllah()
 {
@@ -11,9 +19,10 @@ function BismiAllah()
 function App() {
   return (
     <div>
-      <h1>بسم الله الرحمن الرحيم</h1>
+      <h1 className="bismi_allah">بسم الله الرحمن الرحيم</h1>
       <h1>la ilaha illa Allah mohammed rassoul Allah</h1>
       <BismiAllah />
+      <p style={{ background: bismi_allah.bgc, color: bismi_allah.fgc}}>{bismi_allah.text}</p>
     </div>
   );
 }
