80faf4ff5886e1e1d726b718f2443c661941d3a3
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-06-01 13:27:25 +0100

    astaghfiro Allah

--- libs/zap/bismi_allah/src/bismi_allah.zig
+++ libs/zap/bismi_allah/src/bismi_allah.zig
@@ -3,65 +3,27 @@
 const std = @import("std");
 const zap = @import("zap");
 
-// NOTE: this is a super simplified example, just using a hashmap to map
-// from HTTP path to request function.
-fn dispatch_routes(r: zap.Request) void {
-    // dispatch
-    if (r.path) |the_path| {
-        if (routes.get(the_path)) |foo| {
-            foo(r);
-            return;
-        }
-    }
-    // or default: present menu
-    r.sendBody(
-        \\ <html>
-        \\   <body>
-        \\     <p><a href="/static">static</a></p>
-        \\     <p><a href="/dynamic">dynamic</a></p>
-        \\   </body>
-        \\ </html>
-    ) catch return;
-}
+fn on_request(r: zap.Request) void {
+    r.setStatus(.not_found);
+    std.debug.print("alamdo li Allah\n\n{any}\n", .{r});
+    std.debug.print("method: {s}\n", .{r.method.?});
+    std.debug.print("query: {s}\n", .{if (null != r.query) r.query.? else "null"});
+    std.debug.print("path: {s}\n\n", .{r.path.?});
 
-fn static_site(r: zap.Request) void {
-    r.sendBody("<html><body><h1>Hello from STATIC ZAP!</h1></body></html>") catch return;
+    //alhamdo li Allah
+    //on error 404
+    r.sendBody("<html><head><meta charset=\"utf-8\"></head><body><p>بسم الله الرحمن الرحيم</p><h1>Error: page not found</h1></body></html>") catch return;
 }
-
-var dynamic_counter: i32 = 0;
-fn dynamic_site(r: zap.Request) void {
-    dynamic_counter += 1;
-    var buf: [128]u8 = undefined;
-    const filled_buf = std.fmt.bufPrintZ(
-        &buf,
-        "<html><body><h1>Hello # {d} from DYNAMIC ZAP!!!</h1></body></html>",
-        .{dynamic_counter},
-    ) catch "ERROR";
-    r.sendBody(filled_buf) catch return;
-}
-
-fn setup_routes(a: std.mem.Allocator) !void {
-    routes = std.StringHashMap(zap.HttpRequestFn).init(a);
-    try routes.put("/static", static_site);
-    try routes.put("/dynamic", dynamic_site);
-}
-
-var routes: std.StringHashMap(zap.HttpRequestFn) = undefined;
 
 pub fn main() !void {
     std.debug.print("بسم الله الرحمن الرحيم\n", .{});
-    try setup_routes(std.heap.page_allocator);
+
     var listener = zap.HttpListener.init(.{
         .port = 3000,
-        .on_request = dispatch_routes,
-        .log = true,
+        .on_request = on_request,
+        .public_folder = "public",
     });
     try listener.listen();
 
-    std.debug.print("Listening on 0.0.0.0:3000\n", .{});
-
-    zap.start(.{
-        .threads = 2,
-        .workers = 2,
-    });
+    zap.start(.{ .threads = 1, .workers = 1 });
 }
