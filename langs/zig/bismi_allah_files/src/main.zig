019d247dd3177783f6bcc54319fbbfc2f61f2bf8
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-06-05 12:16:46 +0100

    alhamdo li Allah learned how to write to files in zig

--- langs/zig/bismi_allah_files/src/main.zig
+++ langs/zig/bismi_allah_files/src/main.zig
@@ -13,14 +13,12 @@ test "simple file" {
         std.fs.cwd().deleteFile("bismi_allah.bin") catch {};
     }
 
-    const bytes_written = try file.write("la ilaha illa Allah Mohammed Rassoul Allah");
-    std.debug.print("alhamdo li Allah wrote {d} bytes\n", .{bytes_written});
+    _ = try file.write("la ilaha illa Allah Mohammed Rassoul Allah");
 
     var buffer: [100]u8 = undefined;
     try file.seekTo(0);
     const bytes_read = try file.readAll(&buffer);
 
-    std.debug.print("alhamdo li Allah read {d} bytes\n", .{bytes_read});
     try std.testing.expect(std.mem.eql(u8, buffer[0..bytes_read], "la ilaha illa Allah Mohammed Rassoul Allah"));
 
     {
@@ -34,16 +32,42 @@ test "simple file" {
         bismi_allah.id = 8;
         try file.seekTo(0);
         _ = try file.read(std.mem.asBytes(&bismi_allah.id));
-        std.debug.print("alhamdo li Allah bismi_allah.id == {d}\n", .{bismi_allah.id});
-
-        try file.seekTo(0);
-        const bit: u8 = 1;
-
-        var bit_buffer: @TypeOf(bit) = 0;
-        bit_buffer = bit;
-        std.mem.reverse(u8, std.mem.asBytes(&bit_buffer));
-
-        _ = try file.write(std.mem.asBytes(&bit_buffer));
-        std.debug.print("alhamdo li Allah bit == {d} | bit_buffer == {d}\n", .{ bit, bit_buffer });
+        try std.testing.expect(bismi_allah.id == 99);
     }
+}
+
+test "binary files" {
+    const exepect = std.testing.expect;
+    const file = try std.fs.cwd().createFile("bismi_allah.bin", .{ .read = true });
+    defer {
+        file.close();
+        std.fs.cwd().deleteFile("bismi_allah.bin") catch {};
+    }
+
+    const BismiAllah = struct {
+        id: u32 = 12,
+        name: [34]u8 = undefined,
+        int: u32 = 44,
+        float: f32 = 0.13,
+    };
+    var bismi_allah = BismiAllah{ .id = 99, .name = [_]u8{ 'a', 's', 't', 'a', 'g', 'h', 'f', 'i', 'r', 'o', ' ', 'A', 'l', 'l', 'a', 'h', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' } };
+
+    _ = try file.write(std.mem.asBytes(&bismi_allah.id));
+    _ = try file.write(&bismi_allah.name);
+    _ = try file.write(std.mem.asBytes(&bismi_allah.int));
+    _ = try file.write(std.mem.asBytes(&bismi_allah.float));
+
+    bismi_allah = .{ .id = 0, .int = 0, .float = 99.99 };
+    bismi_allah.name[2] = 'z';
+
+    try file.seekTo(0);
+    _ = try file.read(std.mem.asBytes(&bismi_allah.id));
+    _ = try file.read(&bismi_allah.name);
+    _ = try file.read(std.mem.asBytes(&bismi_allah.int));
+    _ = try file.read(std.mem.asBytes(&bismi_allah.float));
+
+    try exepect(bismi_allah.id == 99);
+    try exepect(bismi_allah.name[2] == 't');
+    try exepect(bismi_allah.int == 44);
+    try exepect(bismi_allah.float == 0.13);
 }
