a8cf5141ab12bf970820fb9865c3db93b42c1e71
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-06-18 12:44:36 +0100

    alhamdo li Allah different values on different releases

--- libs/zap/bismi_allah/src/bismi_allah.zig
+++ libs/zap/bismi_allah/src/bismi_allah.zig
@@ -5,6 +5,8 @@ const zap = @import("zap");
 
 var allocator: std.mem.Allocator = undefined;
 
+var accounts: std.AutoHashMap([64:0]u8, [64:0]u8) = undefined;
+
 fn simplePage(r: zap.Request, body: []const u8) void {
     const template =
         \\<html>
@@ -45,39 +47,7 @@ fn errorPage(r: zap.Request, message: []const u8) void {
     r.sendBody(filled_buffer) catch return;
 }
 
-fn login(r: zap.Request) void {
-    r.parseBody() catch {
-        r.setStatus(.internal_server_error);
-        errorPage(r, "Internal Error");
-    };
-    r.parseQuery();
-    std.debug.print("alhamdo li Allah param count: {d}\n", .{r.getParamCount()});
-
-    std.debug.print("{s}\n", .{r.body orelse ""});
-    const account_name = (r.getParamStr(allocator, "input_account_name", false) catch {
-        r.setStatus(.forbidden);
-        errorPage(r, "Required info were not provided");
-        return;
-    });
-    const account_name_parsed = if (null != account_name) account_name.?.str else "";
-
-    // const account_password = r.getParamSlice("input_account_password") orelse "";
-    const account_password = r.getParamStr(allocator, "input_account_password", false) catch {
-        r.setStatus(.forbidden);
-        errorPage(r, "Required info were not provided");
-        return;
-    };
-    const account_password_parsed = if (null != account_password) account_password.?.str else "";
-
-    std.debug.print("alhamdo li Allah login info was: '{s}' : '{s}'\n", .{ account_name_parsed, account_password_parsed });
-    if (std.mem.eql(u8, "bismi_allah", account_name_parsed) and std.mem.eql(u8, "bismi_allah", account_password_parsed)) {
-        simplePage(r, "<p>connection info was correct</p>");
-    } else {
-        errorPage(r, "connection info incorrect");
-    }
-}
-
-fn on_request(r: zap.Request) void {
+fn onRequest(r: zap.Request) void {
     std.debug.print("===========================================================================\n", .{});
     defer std.debug.print("===========================================================================\n", .{});
 
@@ -89,7 +59,7 @@ fn on_request(r: zap.Request) void {
     std.debug.print("path: {s}\n\n", .{r.path orelse "null"});
 
     if (std.mem.startsWith(u8, r.path.?, "/account")) {
-        request_account(r);
+        requestAccount(r);
     } else if (std.mem.startsWith(u8, r.path.?, "/bismi_allah")) {
         simplePage(r, "");
     } else {
@@ -100,8 +70,8 @@ fn on_request(r: zap.Request) void {
     }
 }
 
-fn request_account(r: zap.Request) void {
-    if (std.mem.startsWith(u8, r.path.?, "/account/login")) {
+fn requestAccount(r: zap.Request) void {
+    if (std.mem.eql(u8, r.path.?, "/account/login")) {
         if (null == r.body) {
             // r.sendFile("public/bismi_allah.html") catch errorPage(r, "internal Error");
             simplePage(r,
@@ -114,9 +84,76 @@ fn request_account(r: zap.Request) void {
             return;
         }
         login(r);
+    } else if (std.mem.eql(u8, r.path.?, "/account/register")) {
+        if (null == r.body) {
+            simplePage(r,
+                \\  <form method="post" action="/account/register">
+                \\      <input name="input_account_name" type="text" placeholder="account name"  maxlength="64"/> <br/>
+                \\      <input name="input_account_password" type="password" placeholder="password"  maxlength="64"/> <br/>
+                \\      <button type="submit">login</button> <br/>
+                \\ </form>
+            );
+            return;
+        }
+        register(r);
     } else {
         simplePage(r, "");
     }
+}
+
+fn login(r: zap.Request) void {
+    r.parseBody() catch {
+        r.setStatus(.internal_server_error);
+        errorPage(r, "Internal Error");
+    };
+    r.parseQuery();
+    std.debug.print("alhamdo li Allah param count: {d}\n", .{r.getParamCount()});
+
+    std.debug.print("{s}\n", .{r.body orelse ""});
+    const account_name = (r.getParamStr(allocator, "input_account_name", false) catch {
+        r.setStatus(.bad_request);
+        errorPage(r, "Required info were not provided");
+        return;
+    });
+    const account_name_parsed = if (null != account_name) account_name.?.str else "";
+
+    // const account_password = r.getParamSlice("input_account_password") orelse "";
+    const account_password = r.getParamStr(allocator, "input_account_password", false) catch {
+        r.setStatus(.bad_request);
+        errorPage(r, "Required info were not provided");
+        return;
+    };
+    const account_password_parsed = if (null != account_password) account_password.?.str else "";
+
+    std.debug.print("alhamdo li Allah {any}\n", .{account_password_parsed});
+    var account_name_buffer: [64:0]u8 = undefined;
+    var account_password_buffer: [64:0]u8 = undefined;
+
+    {
+        var i: usize = 0;
+        while (i < account_name_buffer.len) : (i += 1) account_name_buffer[i] = 0;
+
+        i = 0;
+        while (i < account_password_buffer.len) : (i += 1) account_password_buffer[i] = 0;
+    }
+
+    std.mem.copyForwards(u8, &account_name_buffer, account_name_parsed);
+    std.mem.copyForwards(u8, &account_password_buffer, account_password_parsed);
+
+    if (accounts.get(account_name_buffer)) |real_account_password| {
+        if (std.mem.eql(u8, &real_account_password, &account_password_buffer)) {
+            simplePage(r, "<p>connection info were correct</p>");
+        } else {
+            errorPage(r, "connection info incorrect");
+        }
+    } else {
+        errorPage(r, "connection info incorrect");
+    }
+    std.debug.print("alhamdo li Allah login info buffer were: '{s}' : '{s}'\n", .{ account_name_buffer, account_password_buffer });
+}
+
+fn register(r: zap.Request) void {
+    _ = r;
 }
 
 pub fn main() !void {
@@ -126,9 +163,46 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     allocator = gpa.allocator();
 
+    accounts = std.AutoHashMap([64:0]u8, [64:0]u8).init(allocator);
+    defer accounts.deinit();
+    {
+        var buffer_name: [64:0]u8 = undefined;
+        var buffer_password: [64:0]u8 = undefined;
+
+        {
+            var i: usize = 0;
+            while (i < buffer_name.len) : (i += 1) buffer_name[i] = 0;
+
+            i = 0;
+            while (i < buffer_password.len) : (i += 1) buffer_password[i] = 0;
+        }
+
+        std.mem.copyForwards(u8, &buffer_name, "bismi_allah");
+        std.mem.copyForwards(u8, &buffer_password, "bismi_allah");
+        try accounts.put(buffer_name, buffer_password);
+
+        var password = accounts.get(buffer_name);
+        std.debug.print("alahdmo li Allah name is: '{s}'\n", .{password.?});
+
+        {
+            var i: usize = 0;
+            while (i < buffer_name.len) : (i += 1) buffer_name[i] = 0;
+
+            i = 0;
+            while (i < buffer_password.len) : (i += 1) buffer_password[i] = 0;
+        }
+
+        std.mem.copyForwards(u8, &buffer_name, "alhamdo_li_allah");
+        std.mem.copyForwards(u8, &buffer_password, "alhamdo_li_allah");
+        try accounts.put(buffer_name, buffer_password);
+
+        password = accounts.get(buffer_name);
+        std.debug.print("alahdmo li Allah name is: '{s}'\n", .{password.?});
+    }
+
     var listener = zap.HttpListener.init(.{
         .port = 3000,
-        .on_request = on_request,
+        .on_request = onRequest,
         // .public_folder = "public",
     });
     try listener.listen();
