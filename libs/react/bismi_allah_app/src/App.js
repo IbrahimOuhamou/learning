535da42abe130bf748e0adbe51cc4bc9aadb71b7
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-07-13 20:05:35 +0100

    alhamdo li Allah

--- libs/react/bismi_allah_app/src/App.js
+++ libs/react/bismi_allah_app/src/App.js
@@ -1,36 +1,23 @@
-//بسم الله الرحمن الرحيم
-//la ilaha illa Allah mohammed rassoul Allah
 import logo from './logo.svg';
 import './App.css';
 
-let bismi_allah = {
-  text: 'in the name of Allah',
-  bgc: "black",
-  fgc: "white",
-};
-
-function bismiAllah()
-{
-  alert("بسم الله الرحمن الرحيم");
-}
-
-function BismiAllah()
-{
-    return (
-        <div>
-          <p>alhamdo li Allah</p>
-          <button onClick={bismiAllah}>say bismi Allah</button>
-        </div>
-    );
-}
-
 function App() {
   return (
-    <div>
-      <h1 className="bismi_allah">بسم الله الرحمن الرحيم</h1>
-      <h1>la ilaha illa Allah mohammed rassoul Allah</h1>
-      <BismiAllah />
-      <p style={{ background: bismi_allah.bgc, color: bismi_allah.fgc}}>{bismi_allah.text}</p>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
