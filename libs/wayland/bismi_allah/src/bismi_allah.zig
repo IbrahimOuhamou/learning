a2cf38262c50a2944ec2bb5b7270fe9ef3fbc57a
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-07-20 12:53:36 +0100

    started wayland by the will of Allah

---
+++ libs/wayland/bismi_allah/src/bismi_allah.zig
@@ -0,0 +1,7 @@
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
+const std = @import("std");
+
+pub fn main() !void {
+    std.debug.print("بسم الله الرحمن الرحيم\n", .{});
+}
