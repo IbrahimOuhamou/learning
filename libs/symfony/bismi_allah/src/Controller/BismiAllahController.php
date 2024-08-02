76fae8f4d852023e7e3ec268d5e4bc273403f088
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-08-02 12:30:00 +0100

    alhamdo li Allah added twig stuff by the will of Allah

--- libs/symfony/bismi_allah/src/Controller/BismiAllahController.php
+++ libs/symfony/bismi_allah/src/Controller/BismiAllahController.php
@@ -2,15 +2,27 @@
 // بسم الله الرحمن الرحيم
 // la ilaha illa Allah Mohammed Rassoul Allah
 
+// App\{dir_name}: php standards
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class BismiAllahController {
+class BismiAllahController extends AbstractController {
 
     #[Route('/')]
     public function bismi_allah(): Response {
-        return new Response('<h1>بسم الله الرحمن الرحيم</h1>');
+        $bismi_allah_var = 99;
+        $bismi_allah_asso_arr = [
+            'id' => 1,
+            'name' => 'bismi_allah',
+        ];
+
+        // {controller_name}/{method_name}
+        return $this->render('bismi_allah/bismi_allah.html.twig', [
+            'bismi_allah_var' => $bismi_allah_var,
+            'bismi_allah_asso_arr' => $bismi_allah_asso_arr,
+        ]);
     }
 }
