35c5db00bc52d831bccfcc22ae1778f8aa2a4b27
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-08-09 19:10:20 +0100

    alhamdo li Allah init docker and symfony

---
+++ langs/zig/bismi_allah_builtin/src/bismi_allah.zig
@@ -0,0 +1,8 @@
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
+const std = @import("std");
+const builtin = @import("builtin");
+
+pub fn main() !void {
+    std.debug.print("بسم الله الرحمن الرحيم\n", .{});
+}
