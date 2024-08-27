53c31975ace158373521a83be7bd4d9cc6be4717
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-08-27 19:55:44 +0100

    alamdo li Allah

--- langs/zig/bismi_allah_json/src/bismi_allah.zig
+++ langs/zig/bismi_allah_json/src/bismi_allah.zig
@@ -45,7 +45,7 @@ test "parse low level" {
     scanner.feedInput(
         \\{
         \\  "id": 5,
-        \\  "name" : "bismi Allah",
+        \\  "name" : "bismi Allah"
         \\}
     );
     scanner.endInput();
@@ -64,6 +64,83 @@ test "parse low level" {
 
     std.debug.print("alhamso li Allah id: {d}\n", .{bismi_allah.id});
     std.debug.print("alhamso li Allah name: {s}\n", .{bismi_allah.name});
+}
+
+test "bismi ALlah: parse low level - field" {
+    std.debug.print("bismi Allah: ######################bismi ALlah: parse low level - field#########################\n", .{});
+    defer std.debug.print("###################################################################################\n", .{});
+    const BismiAllah = struct {
+        id: u32 = 0,
+        name: [34]u8 = [1]u8{" "} ** 34,
+    };
+
+    const bismi_allah_json_data =
+        \\{
+        \\  "data": [
+        \\    {
+        \\      "id": 1
+        \\    },
+        \\    {
+        \\      "id": 2
+        \\    }
+        \\  ]
+        \\}
+    ;
+    const allocator = std.testing.allocator;
+
+    var bismi_allah_arr: []BismiAllah = undefined;
+    defer allocator.free(bismi_allah_arr);
+
+    var scanner = std.json.Scanner.initStreaming(allocator);
+    defer scanner.deinit();
+    scanner.feedInput(bismi_allah_json_data);
+    scanner.endInput();
+
+    while (scanner.next()) |token| {
+        std.debug.print("alhamdo li Allah token: '{any}'\n", .{token});
+
+        switch (token) {
+            .end_of_document => break,
+            .string => {
+                if (std.mem.eql(u8, "data", token.string)) {
+                    try loadDataField(&scanner, &bismi_allah_arr, allocator);
+                }
+            },
+            else => {},
+        }
+    } else |err| {
+        std.log.err("alhamdo li Allah error '{any}'\n", .{err});
+    }
+
+    std.debug.print("\nalamdo li Allah result: \n", .{});
+    for (bismi_allah_arr, 0..) |bismi_allah_object, i| {
+        std.debug.print("{d}: id: '{d}', name: '{s}'\n", .{ i, bismi_allah_object.id, bismi_allah_object.name });
+    }
+}
+
+fn loadDataField(scanner: *std.json.Scanner, bismi_allah_arr: anytype, allocator: std.mem.Allocator) !void {
+    bismi_allah_arr.* = try allocator.alloc(@TypeOf(bismi_allah_arr.*[0]), 1);
+    var index: usize = 0;
+
+    while (scanner.next()) |token| {
+        std.debug.print("alhamdo li Allah token: '{any}'\n", .{token});
+
+        switch (token) {
+            .array_end => break,
+            .object_begin => {
+                bismi_allah_arr.* = try allocator.realloc(bismi_allah_arr.*, index + 1);
+                std.debug.print("alamdo li Allah len: '{d}'\n", .{bismi_allah_arr.len});
+            },
+            .object_end => index += 1,
+            .string => {
+                if (std.mem.eql(u8, token.string, "id")) {
+                    const token_data = try scanner.next();
+                    bismi_allah_arr.*[bismi_allah_arr.len - 1].id = try std.fmt.parseInt(u32, token_data.number, 10);
+                }
+            },
+            else => {},
+        }
+    } else |err| return err;
 }
 
 test "bismi Allah stringify" {
