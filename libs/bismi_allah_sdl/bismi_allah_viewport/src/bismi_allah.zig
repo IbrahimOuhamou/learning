1d7ea22f5e950c908231d824fcc645cd067160e5
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-08-30 11:13:05 +0100

    alhamdo li Allah added SDL_Viewport

--- libs/bismi_allah_sdl/bismi_allah_viewport/src/bismi_allah.zig
+++ libs/bismi_allah_sdl/bismi_allah_viewport/src/bismi_allah.zig
@@ -1,9 +1,13 @@
 // بسم الله الرحمن الرحيم
 // la ilaha illa Allah Mohammed Rassoul Allah
 
+const std = @import("std");
 const c = @cImport({
     @cInclude("SDL2/SDL.h");
 });
+
+var rect: c.SDL_Rect = .{ .x = 30, .y = 30, .w = 30, .h = 30 };
+var viewport: c.SDL_Rect = .{ .x = -30, .y = 0, .w = 400, .h = 140 };
 
 pub fn main() !void {
     if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
@@ -12,7 +16,7 @@ pub fn main() !void {
     }
     defer c.SDL_Quit();
 
-    const screen = c.SDL_CreateWindow("My Game Window", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 400, 140, c.SDL_WINDOW_OPENGL) orelse
+    const screen = c.SDL_CreateWindow("bismi Allah", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 400, 140, c.SDL_WINDOW_OPENGL) orelse
         {
         c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
         return error.SDLInitializationFailed;
@@ -33,11 +37,24 @@ pub fn main() !void {
                 c.SDL_QUIT => {
                     quit = true;
                 },
+                c.SDL_KEYDOWN => {
+                    // std.debug.print("alhamdo li Allah event.key: '{any}'\n", .{event.key});
+                    if (event.key.keysym.scancode == c.SDL_SCANCODE_LEFT) viewport.x += 10;
+
+                    const keys = c.SDL_GetKeyboardState(null);
+                    if (0 != keys[c.SDL_SCANCODE_RIGHT]) viewport.x -= 10;
+                },
                 else => {},
             }
         }
 
+        _ = c.SDL_SetRenderDrawColor(renderer, 100, 100, 100, 255);
         _ = c.SDL_RenderClear(renderer);
-        c.SDL_RenderPresent(renderer);
+        defer c.SDL_RenderPresent(renderer);
+
+        _ = c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        _ = c.SDL_RenderFillRect(renderer, &rect);
+
+        _ = c.SDL_RenderSetViewport(renderer, &viewport);
     }
 }
