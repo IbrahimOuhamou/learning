b0288885134547896c2c638536f22fad5fadde16
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-09-25 19:12:59 +0100

    astaghfiro Allah

---
+++ langs/php/bismi_allah_url.php
@@ -0,0 +1,10 @@
+<?php
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed rassoul Allah
+
+echo "بسم الله الرحمن الرحيم\n";
+
+$bismi_allah_url = 'https://sub-domain';
+
+var_dump(filter_var($bismi_allah_url, FILTER_VALIDATE_URL));
+
