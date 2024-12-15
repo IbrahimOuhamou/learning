7646252aa39c40789fe886a48eb9ce139162b9fd
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-12-15 14:53:13 +0100

    alamdo li Allah single dikr app

--- libs/bismi_allah_flet/bismi_allah/main.py
+++ libs/bismi_allah_flet/bismi_allah/main.py
@@ -5,7 +5,23 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "بسم الله الرحمن الرحيم"
-    page.add(ft.SafeArea(ft.Text("بسم الله الرحمن الرحيم")))
+    page.window.frameless = True
+
+    def countUp(e):
+        button_counter.text = str(int(button_counter.text) + 1)
+        page.update()
+
+    text_remembrance = ft.Text(value="سبحان الله وبحمده")
+    button_counter = ft.ElevatedButton(text="0", on_click=countUp)
+
+    page.add(
+        ft.Column(
+            [
+                text_remembrance,
+                button_counter,
+            ],
+        )
+    )
 
 
 ft.app(main)
