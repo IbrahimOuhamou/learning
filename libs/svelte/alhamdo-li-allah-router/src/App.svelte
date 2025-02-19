1b49f3b9f49ecfcc1ac1c05fa2b754ce28feed79
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-02-19 18:17:49 +0100

    la ilaha illa Allah

---
+++ libs/svelte/alhamdo-li-allah-router/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  // import type { Route } from "@mateothegreat/svelte5-router";
+  import { route, Router } from "@mateothegreat/svelte5-router";
+  import HomePage from './lib/HomePage.svelte';
+  import About from './lib/About.svelte';
+
+  const routes = [
+    {
+      path: "/",
+      component: HomePage
+    },
+    {
+      path: "about",
+      component: About
+    },
+  ];
+</script>
+
+<div class="flex gap-2">
+  <a use:route href="/">Home</a>
+  <a use:route href="/about">About</a>
+</div>
+
+<p class="text-green-800">la ilaha illa Allah Mohammed Rassoul Allah</p>
+
+<Router base="/" {routes} />
+
