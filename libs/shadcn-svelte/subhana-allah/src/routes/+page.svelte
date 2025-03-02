dc6b0cee1fa41c9a7e6d9f31fd6b2f850a216dd8
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-02 12:34:59 +0000

    thanks to Allah

--- libs/shadcn-svelte/subhana-allah/src/routes/+page.svelte
+++ libs/shadcn-svelte/subhana-allah/src/routes/+page.svelte
@@ -1,10 +1,18 @@
 <script>
-import { Button } from "$lib/components/ui/button/index.js";
+	import { Button } from "$lib/components/ui/button/index";
+	import { Badge } from "$lib/components/ui/badge/index";
+	import { Calendar } from "$lib/components/ui/calendar/index";
+	let value = $state('');
 </script>
 <h1 class="text-2xl">بسم الله الرحمن الرحيم</h1>
 <p>
 	la ilaha illa Allah Mohammed Rassoul Allah
 </p>
+
+
 <Button size="lg">
 	بسم الله الرحمن الرحيم
 </Button>
+<Badge>badge</Badge>
+<Calendar bind:value={value} />
+<Badge>{value}</Badge>
