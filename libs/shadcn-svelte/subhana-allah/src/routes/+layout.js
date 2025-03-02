8d68f306704835ac6266f2f1daf4f8b3413fde90
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-02 12:01:20 +0000

    init shadcn-svelte thanks to Allah

---
+++ libs/shadcn-svelte/subhana-allah/src/routes/+layout.js
@@ -0,0 +1,4 @@
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
+export const prerender = true;
+export const ssr = false;
---
+++ libs/shadcn-svelte/subhana-allah/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+import { Button } from "$lib/components/ui/button/index.js";
+</script>
+<h1 class="text-2xl">بسم الله الرحمن الرحيم</h1>
+<p>
+	la ilaha illa Allah Mohammed Rassoul Allah
+</p>
+<Button size="lg">
+	بسم الله الرحمن الرحيم
+</Button>
