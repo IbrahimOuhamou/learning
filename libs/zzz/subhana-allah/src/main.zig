6fa0b7d95941f204c5d251147c081ccb9d03c5e5
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-26 11:55:01 +0000

    la ilaha illa Allah

--- libs/zzz/subhana-allah/src/main.zig
+++ libs/zzz/subhana-allah/src/main.zig
@@ -1,46 +1,70 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
+const std = @import("std");
+const log = std.log.scoped(.@"examples/basic");
+
+const zzz = @import("zzz");
+const http = zzz.HTTP;
+
+const tardy = zzz.tardy;
+const Tardy = tardy.Tardy(.auto);
+const Runtime = tardy.Runtime;
+const Socket = tardy.Socket;
+
+const Server = http.Server;
+const Router = http.Router;
+const Context = http.Context;
+const Route = http.Route;
+const Respond = http.Respond;
+
+fn base_handler(ctx: *const Context, _: void) !Respond {
+    return ctx.response.apply(.{
+        .status = .OK,
+        .mime = http.Mime.HTML,
+        .body = "Hello, world!",
+    });
+}
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    const host: []const u8 = "0.0.0.0";
+    const port: u16 = 9862;
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var gpa = std.heap.GeneralPurposeAllocator(.{ .thread_safe = true }){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var t = try Tardy.init(allocator, .{ .threading = .auto });
+    defer t.deinit();
 
-    try bw.flush(); // Don't forget to flush!
-}
+    var router = try Router.init(allocator, &.{
+        Route.init("/").get({}, base_handler).layer(),
+    }, .{});
+    defer router.deinit(allocator);
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
+    var socket = try Socket.init(.{ .tcp = .{ .host = host, .port = port } });
+    defer socket.close_blocking();
+    try socket.bind();
+    try socket.listen(4096);
 
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    const EntryParams = struct {
+        router: *const Router,
+        socket: Socket,
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+
+    std.debug.print("alhamdo li Allah listening on '{s}':'{d}'\n", .{ host, port });
+
+    try t.entry(
+        EntryParams{ .router = &router, .socket = socket },
+        struct {
+            fn entry(rt: *Runtime, p: EntryParams) !void {
+                var server = Server.init(.{
+                    .stack_size = 1024 * 1024 * 4,
+                    .socket_buffer_bytes = 1024 * 2,
+                    .keepalive_count_max = null,
+                    .connection_count_max = 1024,
+                });
+                try server.serve(rt, p.router, .{ .normal = p.socket });
+            }
+        }.entry,
+    );
 }
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("subhana_allah_lib");
