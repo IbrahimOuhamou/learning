c5dbd103519a6277d90084d32ba68b6aa8b3d835
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-03-31 13:48:06 +0000

    astaghfiro Allah

--- libs/sqlt/subhana-allah/src/main.zig
+++ libs/sqlt/subhana-allah/src/main.zig
@@ -1,46 +1,70 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+// بسم الله الرحمن الرحيم
+// la ilaha illa Allah Mohammed Rassoul Allah
+
+const std = @import("std");
+const sqlt = @import("sqlt");
+
+const Sqlite = sqlt.Sqlite;
+
+const User = struct {
+    const Permission = enum(u32) { none = 0, admin = 1 };
+    const Country = enum { malysia, syria };
+
+    name: []const u8,
+    perms: Permission,
+    country: Country,
+    age: ?i32,
+    weight: f32 = 10.0,
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
+    try stdout.print("بسم الله الرحمن الرحيم\n", .{});
     try bw.flush(); // Don't forget to flush!
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    const conn = try Sqlite.open(":memory:");
+    defer conn.close();
+
+    try sqlt.migrate(allocator, conn);
+
+    try conn.execute(
+        \\insert into users (name, age, perms, country) values (?, ?, ?, ?)
+    , .{ "Alice", 25, User.Permission.admin, User.Country.syria });
+
+    try conn.execute(
+        \\insert into users (name, age, perms, country) values (?, ?, ?, ?)
+    , .{ "Jane", 99, User.Permission.none, User.Country.malysia });
+
+    try conn.execute(
+        \\insert into users (name, age, perms, country) values (?, ?, ?, ?)
+    , .{ "Girl", 7, User.Permission.none, User.Country.syria });
+
+    try conn.execute(
+        \\insert into users (name, age, perms, country) values (?, ?, ?, ?)
+    , .{ "Adam", null, User.Permission.none, User.Country.malysia });
+
+    const john = try conn.fetch_optional(allocator, User,
+        \\ select name, age, perms, country from users
+        \\ where name = ?
+    , .{"John"});
+
+    const all_users = try conn.fetch_all(allocator, User, "select * from users", .{});
+
+    defer allocator.free(all_users);
+    defer for (all_users) |user| allocator.free(user.name);
+
+    std.debug.print("john is {?}\n", .{john});
+
+    for (all_users) |user| try stdout.print(
+        "{s}'s age: {?d} + weight: {?d} | perms: {s} + country: {s}\n",
+        .{ user.name, user.age, user.weight, @tagName(user.perms), @tagName(user.country) },
+    );
+    try bw.flush();
 }
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
-const std = @import("std");
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("subhana_allah_lib");
