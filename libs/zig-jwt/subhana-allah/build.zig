9bad9a0929529847b56d81115e2c7786273b163e
IbrahimOuhamou <ouhamouy10@gmail.com>
2025-04-08 16:24:46 +0100

    Allah akbar

--- libs/zig-jwt/subhana-allah/build.zig
+++ libs/zig-jwt/subhana-allah/build.zig
@@ -17,18 +17,30 @@ pub fn build(b: *std.Build) void {
     // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const jwt = b.dependency("jwt", .{
+    const jwt_dep = b.dependency("jwt", .{
         .target = target,
         .optimize = optimize,
-    }).module("jwt");
+    });
+    const jwt_lib = jwt_dep.module("jwt");
 
-    const exe = b.addExecutable(.{
-        .name = "subhana-allah",
+    // We will also create a module for our other entry point, 'main.zig'.
+    const exe_mod = b.createModule(.{
+        // `root_source_file` is the Zig "entry point" of the module. If a module
+        // only contains e.g. external object files, you can make this `null`.
+        // In this case the main source file is merely a path, however, in more
+        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-    exe.root_module.addImport("jwt", jwt);
+    exe_mod.addImport("jwt", jwt_lib);
+
+    // This creates another `std.Build.Step.Compile`, but this one builds an executable
+    // rather than a static library.
+    const exe = b.addExecutable(.{
+        .name = "subhana_allah",
+        .root_module = exe_mod,
+    });
 
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
@@ -59,9 +71,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
+        .root_module = exe_mod,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
